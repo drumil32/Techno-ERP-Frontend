e1df486667d037ac7d892618acc54ff4d210957a
Rajat Nai <rajatnai49@gmail.com>
2025-03-12 10:04:13 +0530

    feat: login page added

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,29 +2,37 @@ import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "@/lib/queryClient";
+
+
 const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin']
+    variable: '--font-geist-sans',
+    subsets: ['latin']
 });
 
 const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin']
+    variable: '--font-geist-mono',
+    subsets: ['latin']
 });
 
 export const metadata: Metadata = {
-  title: 'Techno ERP',
-  description: 'erp system for college.'
+    title: 'Techno ERP',
+    description: 'erp system for college.'
 };
 
 export default function RootLayout({
-  children
+    children
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+                <QueryClientProvider client={queryClient}>
+                    {children}
+                </QueryClientProvider>
+            </body>
+        </html>
+    );
 }
