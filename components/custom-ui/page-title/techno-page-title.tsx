58da3a12d32526abf8d8bbf55af6cd746baefc6e
kunjgit <patelkunj2021@gmail.com>
2025-03-23 06:19:39 +0000

    [Fix]: font config & toaster update with conflict resolution

--- components/custom-ui/page-title/techno-page-title.tsx
+++ components/custom-ui/page-title/techno-page-title.tsx
@@ -4,6 +4,6 @@ export default function TechnoPageTitle() {
   const { headerActiveItem } = useTopHeaderContext();
 
   return (
-    <div className="my-4 font-black tracking-wider text-3xl text-gray-700">{headerActiveItem}</div>
+    <div className="my-4 font-[700]  text-3xl text-[#4E4E4E]">{headerActiveItem}</div>
   );
 }
