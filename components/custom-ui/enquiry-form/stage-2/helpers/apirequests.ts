b81a080aeede40b2572c178a8d8e702c9e332033
Rajat Nai <rajatnai49@gmail.com>
2025-04-01 21:38:11 +0530

    [Feat]: Add Strcture of Step - 2

--- components/custom-ui/enquiry-form/stage-2/helpers/apirequests.ts
+++ components/custom-ui/enquiry-form/stage-2/helpers/apirequests.ts
@@ -2,18 +2,32 @@ import { API_ENDPOINTS } from "@/common/constants/apiEndpoints";
 import { API_METHODS } from "@/common/constants/apiMethods";
 
 export const getOtherFees = async () => {
-    const response = await fetch(API_ENDPOINTS.getOtherFees, {
-      method: API_METHODS.GET,
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      credentials: 'include'
-    });
-    if (!response.ok) {
-      throw new Error("Failed to fetch other fees");
-    }
-  
-    return response.json();
+  const response = await fetch(API_ENDPOINTS.getOtherFees, {
+    method: API_METHODS.GET,
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    credentials: 'include'
+  });
+  if (!response.ok) {
+    throw new Error("Failed to fetch other fees");
   }
-  
-  
+
+  return response.json();
+}
+
+
+export const getFeesByCourseName = async (course_name: string) => {
+  const response = await fetch(API_ENDPOINTS.getFeesByCourse(course_name), {
+    method: API_METHODS.GET,
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    credentials: 'include'
+  });
+  if (!response.ok) {
+    throw new Error("Failed to fetch other fees");
+  }
+
+  return response.json();
+}
