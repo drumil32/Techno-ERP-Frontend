2fb1137fce3f1ffbbf12bd8f2c9464844efa3b12
Rajat Nai <rajatnai49@gmail.com>
2025-05-07 08:07:50 +0530

    [Fix]: Depost - Refund total calculation | table overflow fix

--- components/layout/finance-student-dues/details-tables/all-transaction-details.tsx
+++ components/layout/finance-student-dues/details-tables/all-transaction-details.tsx
@@ -36,7 +36,11 @@ export default function AllTransactionsDetails({
 
   const amountTotal = transactionHistory.reduce(
     (totals, item) => {
-      totals.amount += item.amount ?? 0;
+      if (item.feeAction == FeeActions.DEPOSIT) {
+        totals.amount += item.amount ?? 0;
+      } else {
+        totals.amount -= item.amount ?? 0
+      }
       return totals;
     },
     { amount: 0 }
@@ -47,60 +51,62 @@ export default function AllTransactionsDetails({
       <div className="w-full flex p-2 items-center">
         <div className="font-semibold text-[16px]">All Transactions </div>
       </div>
-      <Table className="w-full">
-        <TableHeader className="bg-[#5B31D1]/10 backdrop-blur-lg [&_th]:!text-[#5B31D1]">
-          <TableRow>
-            <TableHead className="rounded-l-[5px]">S. No</TableHead>
-            <TableHead className="w-[100px]">Date</TableHead>
-            <TableHead className="w-[80px]">Time</TableHead>
-            <TableHead className="w-[110px]">Transaction ID</TableHead>
-            <TableHead className="w-[120px]">Fees Action</TableHead>
-            <TableHead className="w-[80px] text-right">Amount</TableHead>
-            <TableHead className="w-[110px]">Transaction Type</TableHead>
-            <TableHead className="w-auto rounded-r-[5px]">Remarks</TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {extendedTransactionHistory.map((transaction) => (
-            <TableRow key={transaction._id}>
-              <TableCell className="w-[31px]">{transaction.sno}</TableCell>
-              <TableCell className="w-[80px]">
-                {format(transaction.dateTime, 'dd/MM/yyyy')}
+      <div className="w-full overflow-x-auto">
+        <Table className="w-full">
+          <TableHeader className="bg-[#5B31D1]/10 backdrop-blur-lg [&_th]:!text-[#5B31D1]">
+            <TableRow>
+              <TableHead className="rounded-l-[5px]">S. No</TableHead>
+              <TableHead className="w-[100px]">Date</TableHead>
+              <TableHead className="w-[80px]">Time</TableHead>
+              <TableHead className="w-[110px]">Transaction ID</TableHead>
+              <TableHead className="w-[120px]">Fees Action</TableHead>
+              <TableHead className="w-[80px] text-right">Amount</TableHead>
+              <TableHead className="w-[110px]">Transaction Type</TableHead>
+              <TableHead className="w-auto rounded-r-[5px]">Remarks</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {extendedTransactionHistory.map((transaction) => (
+              <TableRow key={transaction._id}>
+                <TableCell className="w-[31px]">{transaction.sno}</TableCell>
+                <TableCell className="w-[80px]">
+                  {format(transaction.dateTime, 'dd/MM/yyyy')}
+                </TableCell>
+                <TableCell className="w-[100px]">{format(transaction.dateTime, 'HH:mm')}</TableCell>
+                <TableCell className="w-[80px]">{transaction.transactionID}</TableCell>
+                <TableCell className="w-[110px]">
+                  <FeeActionTag status={transaction.feeAction as FeeActions} />
+                </TableCell>
+                <TableCell className="w-[120px] text-right">
+                  {transaction.amount != null ? `₹ ${transaction.amount.toLocaleString()}` : '__'}
+                </TableCell>
+                <TableCell className="w-[110px]">
+                  <TxnTypaTag status={transaction.txnType as TransactionTypes} />
+                </TableCell>
+                <TableCell className="">
+                  <TruncatedCell
+                    value={transaction.remark === '' ? '--' : transaction.remark}
+                    maxWidth={300}
+                  />
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+          <TableFooter>
+            <TableRow>
+              <TableCell className="rounded-l-[5px]" colSpan={5}>
+                Total
               </TableCell>
-              <TableCell className="w-[100px]">{format(transaction.dateTime, 'HH:mm')}</TableCell>
-              <TableCell className="w-[80px]">{transaction.transactionID}</TableCell>
-              <TableCell className="w-[110px]">
-                <FeeActionTag status={transaction.feeAction as FeeActions} />
+              <TableCell className="rounded-r-[5px] text-right">
+                ₹{amountTotal?.amount.toLocaleString()}
               </TableCell>
-              <TableCell className="w-[120px] text-right">
-                {transaction.amount != null ? `₹ ${transaction.amount.toLocaleString()}` : '__'}
-              </TableCell>
-              <TableCell className="w-[110px]">
-                <TxnTypaTag status={transaction.txnType as TransactionTypes} />
-              </TableCell>
-              <TableCell className="">
-                <TruncatedCell
-                  value={transaction.remark === '' ? '--' : transaction.remark}
-                  maxWidth={300}
-                />
+              <TableCell className="rounded-r-[5px]" colSpan={2}>
+                {''}
               </TableCell>
             </TableRow>
-          ))}
-        </TableBody>
-        <TableFooter>
-          <TableRow>
-            <TableCell className="rounded-l-[5px]" colSpan={5}>
-              Total
-            </TableCell>
-            <TableCell className="rounded-r-[5px] text-right">
-              ₹{amountTotal?.amount.toLocaleString()}
-            </TableCell>
-            <TableCell className="rounded-r-[5px]" colSpan={2}>
-              {''}
-            </TableCell>
-          </TableRow>
-        </TableFooter>
-      </Table>
+          </TableFooter>
+        </Table>
+      </div>
     </div>
   );
 }
