7b7563934cb4d61da10fa1103b2f5b537fe58ca0
kunjgit <patelkunj2021@gmail.com>
2025-06-04 08:46:15 +0530

    [Fix]:Meta tags fixed

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,6 @@ import ProgressBar from '@/components/custom-ui/progress-bar/progress-bar';
 import { NavigationEvents } from '@/components/custom-ui/router-events/router-event';
 import { Suspense } from 'react';
 import { DevBadge } from '@/components/ui/dev-badge';
-import Head from 'next/head';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -29,18 +28,13 @@ const inter = Inter({
 export const metadata: Metadata = {
   title: 'Techno ERP',
   description: 'Enterprise-Resource-Planning for Techno',
+  metadataBase: new URL('https://techno.sprintup.in'),
   openGraph: {
     title: 'Techno ERP',
-    description: 'Enterprise-Resource-Planning for Techno',
-    url: 'https://techno.sprintup.in',
-    siteName: 'Techno ERP',
-    images: [
-      {
-        url: 'https://nextjs.org/og.png',
-        width: 800,
-        height: 600
-      }
-    ],
+    description:
+      'A powerful all-in-one Lead Tracking and ERP solution built for ambitious colleges.',
+    images: '/og-image.png',
+    url: 'https://example.com',
     locale: 'en_US',
     type: 'website'
   }
@@ -53,35 +47,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <Head>
-        <meta
-          name="description"
-          content="A powerful all-in-one Lead Tracking and ERP solution built for ambitious colleges. We empower institutions to streamline admissions, enhance academic quality, optimize faculty workload, and maintain data seamlessly — all in one integrated platform."
-        />
-
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://techno.sprintup.in" />
-        <meta property="og:title" content="Techno ERP" />
-        <meta
-          property="og:description"
-          content="A powerful all-in-one Lead Tracking and ERP solution built for ambitious colleges."
-        />
-        <meta property="og:site_name" content="Techno ERP" />
-        <meta property="og:image" content="https://techno.sprintup.in/favicon.ico" />
-        <meta property="og:image:width" content="1200" />
-        <meta property="og:image:height" content="630" />
-        <meta property="og:image:alt" content="Techno ERP Preview" />
-
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content="Techno ERP" />
-        <meta
-          name="twitter:description"
-          content="A powerful all-in-one Lead Tracking and ERP solution built for ambitious colleges."
-        />
-        <meta name="twitter:image" content="https://techno.sprintup.in/favicon.ico" />
-
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+      <head></head>
       <body
         className={`overflow-hidden ${geistSans.variable} ${geistMono.variable} ${inter.variable} antialiased`}
       >
