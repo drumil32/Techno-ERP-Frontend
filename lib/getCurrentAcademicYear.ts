fd0ce62c4359b0271ced5c9be03498761cc9d81d
drumil32 <akheniad@gmail.com>
2025-06-21 00:13:12 +0530

    dropdown fix

--- lib/getCurrentAcademicYear.ts
+++ lib/getCurrentAcademicYear.ts
@@ -5,3 +5,11 @@ export const getCurrentAcademicYear = () => {
 
     return currentMonth>=6 ? `${currentYear}-${(currentYear + 1).toString()}` : `${currentYear - 1}-${currentYear.toString()}`;
 }
+
+export const getCurrentAcademicYearForTemp = () => {
+    const currentDate = new Date();
+    const currentMonth = currentDate.getMonth();
+    const currentYear = currentDate.getFullYear();
+
+    return currentMonth>=5 ? `${currentYear}-${(currentYear + 1).toString()}` : `${currentYear - 1}-${currentYear.toString()}`;
+}
